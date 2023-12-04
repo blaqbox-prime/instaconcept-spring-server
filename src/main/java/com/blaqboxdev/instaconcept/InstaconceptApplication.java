7fbb1ebb50adb9802589a7a744ad4ee4ca10b7e3
Karabo Sambo <karabosambo@outlook.com>
2023-12-04 19:21:02 +0200

    Initial Setup + db connection

---
+++ src/main/java/com/blaqboxdev/instaconcept/InstaconceptApplication.java
@@ -0,0 +1,13 @@
+package com.blaqboxdev.instaconcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaconceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaconceptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blaqboxdev/instaconcept/InstaconceptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blaqboxdev.instaconcept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaconceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
